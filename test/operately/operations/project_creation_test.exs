dcba14c66bfb7bdc93a5d8d511c2f8b2267b40df
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-26 12:34:54 -0300

    ProjectCreation operation creates access bindings
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/operately/operations/project_creation_test.exs
+++ test/operately/operations/project_creation_test.exs
@@ -1,21 +1,32 @@
 defmodule Operately.Operations.ProjectCreationTest do
   use Operately.DataCase
 
+  import Ecto.Query, only: [from: 2]
+
   import Operately.CompaniesFixtures
   import Operately.PeopleFixtures
   import Operately.GroupsFixtures
   import Operately.GoalsFixtures
+  import Operately.AccessFixtures, only: [group_for_person_fixture: 1]
 
   alias Operately.Repo
   alias Operately.Access
+  alias Operately.Access.Binding
   alias Operately.Projects
   alias Operately.Projects.Project
 
   setup do
     company = company_fixture()
+
     creator = person_fixture_with_account(%{company_id: company.id})
+    group_for_person_fixture(creator)
+
     champion = person_fixture_with_account(%{company_id: company.id})
+    group_for_person_fixture(champion)
+
     reviewer = person_fixture_with_account(%{company_id: company.id})
+    group_for_person_fixture(reviewer)
+
     group = group_fixture(creator)
     goal = goal_fixture(creator, %{space_id: group.id, targets: []})
 
@@ -87,5 +98,37 @@ defmodule Operately.Operations.ProjectCreationTest do
     |> Operately.Operations.ProjectCreation.run()
 
     assert nil != Access.get_context!(project_id: project.id)
+  end
+
+  test "ProjectCreation operation creates access bindings for champion, reviewer and creator", ctx do
+    {:ok, project} = %Operately.Operations.ProjectCreation{
+      company_id: ctx.company.id,
+      name: "project 1",
+      champion_id: ctx.champion.id,
+      reviewer_id: ctx.reviewer.id,
+      creator_id: ctx.creator.id,
+      creator_role: "manager",
+      creator_is_contributor: "yes",
+      visibility: "invite",
+      group_id: ctx.group.id,
+      goal_id: ctx.goal.id,
+    }
+    |> Operately.Operations.ProjectCreation.run()
+
+    assert nil != fetch_access_binding(project, ctx.creator, 70)
+    assert nil != fetch_access_binding(project, ctx.champion, 100)
+    assert nil != fetch_access_binding(project, ctx.reviewer, 100)
+  end
+
+  #
+  # Helpers
+  #
+
+  defp fetch_access_binding(project, person, access_level) do
+    context = Access.get_context!(project_id: project.id)
+    group = Access.get_person_group(person)
+
+    from(b in Binding, where: b.access_context_id == ^context.id and b.access_group_id == ^group.id and b.access_level == ^access_level)
+    |> Repo.one()
   end
 end
